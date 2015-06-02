6ed8a465ec479d4f7c0cfdfc13ad1dc3f9f4d904
elwan7 <elwan7@gmail.com>
2015-06-02 14:11:52 +0000

    time system attendance python

--- import_file.py
+++ import_file.py
@@ -1,11 +1,21 @@
+#-*-encoding: utf-8 -*-
+#-*-encoding:latin-1-*-
 import csv
-with open ('Test_Fichier_Upload_KFK_1.csv',newline='') as f:
+
+
+with open ('Test_Fichier_Upload_KFK_1.csv',newline='',encoding='utf8') as f:
     #reader = csv.reader(f)
     reader = csv.DictReader(f)
     
     #for row in reader:
     #    print(row['id'],row['credit'],row['debit'])
     for row in reader:
-       # for key,values in row.items():
-        #    print (key,values)
-        print (row)   
+        for key,values in row.items():
+            print (key,values)
+            
+        #if row is not None:
+        #    print ("Ok")
+        #else:
+        #    print("NOK")
+            
+        
