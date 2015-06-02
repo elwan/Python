8abebacfe9782827d00ac9664f93fdd6a40c44d3
elwan7 <elwan7@gmail.com>
2015-06-02 14:13:54 +0000

    time attendance system with python

--- pointage.py
+++ pointage.py
@@ -1,7 +1,10 @@
+#python3.4
+#test program for system time attendance 
 import datetime
-from time import time
-from time import sleep
+from time import time sleep
 
+
+#create employee object
 class Employee(object):
     def __init__(self,name,prenom,age,date_debut,code):
         self.nom=name
@@ -15,38 +18,41 @@ class Employee(object):
         self.code=''
         self.shift_normal=0
         self.heure_suplementaire=0
-
+#employee login 
     def is_in(self):
         self.temps_arrivee = time()
         self.attendance_in =self.temps_arrivee
         return self.temps_arrivee
+#emplotee logout
     def is_out(self):
         self.temps_sortie = time()
-        
         self.attendance_out =self.temps_sortie
         return self.temps_sortie
+#save log in file 
     def log(self):
         pass
-    
+
+#validate a attendance 
     def validation_shift(self):
-        temps_shift=self.temps_sortie - self.temps_arrivee
-        if  temps_shift <=20:
+        temps_shift=self.temps_sortie - self.temps_arrivee #doing difference with arrival time et departure time in seconde
+        if  temps_shift <=20: #test if the difference is between 0-20
             return True
         else:
-            self.shift_normal=20
-            self.heure_suplementaire =temps_shift - self.shift_normal
+            self.shift_normal=20 #if the difference is greater than 20 hold this variable to 20 
+            self.heure_suplementaire =temps_shift - self.shift_normal #calcute the addional time before a normal time 
             #print (" vous avez fait {} d'heures suplemetaires".format(self.heure_suplementaire))
             return False
             
     def affiche_temps(self):
-        if self.validation_shift():
+        if self.validation_shift(): #if validation is correcte show information 
              if self.temps_sortie > self.temps_arrivee:
                   self.pointage= self.temps_sortie - self.temps_arrivee
                   print( "temps de votre shift est de: {} ".format(self.pointage))
-        else:
+        else:#else show a additional time 
             print (" vous avez fait {} d'heures suplemetaires".format(self.heure_suplementaire))
             
 
+            
 test=Employee("Ndiaye","elwan",27,'01/11/2014',"elwan7")
 
 test.is_in()
